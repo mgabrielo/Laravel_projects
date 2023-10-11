4d9f91d49e739cfd0c415b90198a80d03314484c
Scott Nitro <scottnitro1@gmail.com>
2023-10-11 12:18:37 +0100

    3rd commit

--- Livewire-Ecommerce/EcommerceShop/routes/web.php
+++ Livewire-Ecommerce/EcommerceShop/routes/web.php
@@ -5,28 +5,25 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
 /*
-|--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
 */
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-
 Route::get('/', HomeComponent::class)->name('home.index');
 Route::get('/shop', ShopComponent::class)->name('shop');
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::get('/cart', CartComponent::class)->name('shop.cart');
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 
